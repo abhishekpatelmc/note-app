7183f9ec447102fce78fb5d44f7fb4c3f9de7892
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2024-07-06 00:13:45 -0400

    feat: Add toggle functionality to show/hide details in Home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import "./Home.css";
 
 const Home = () => {
+  const [todoVisible, setTodoVisible] = useState(false);
+  const [inporgressVisible, setInprogressVisible] = useState(false);
+  const [doneVisbile, setDoneVisible] = useState(false);
+
   return (
     <div>
       <h1>Note</h1>
       <div className="homeList">
-        <button className="homeList_item">Todo</button>
-        <button className="homeList_item">In Progress</button>
-        <button className="homeList_item">Done</button>
+        <div className="homeList_item">
+          <button onClick={() => setTodoVisible(!todoVisible)}>Todo</button>
+          {todoVisible && (
+            <div className="homeList_item_details">To do list</div>
+          )}
+        </div>
+
+        <div className="homeList_item">
+          <button onClick={() => setInprogressVisible(!inporgressVisible)}>
+            In Progress
+          </button>
+          {inporgressVisible && (
+            <div className="homeList_item_details">In Progress</div>
+          )}
+        </div>
+        <div className="homeList_item">
+          <button onClick={() => setDoneVisible(!doneVisbile)}>Done</button>
+          {doneVisbile && <div className="homeList_item_details">Done</div>}
+        </div>
       </div>
     </div>
   );
