9402728f8d0e640b45b60814ccdd6eed5dce8e4f
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2024-07-18 20:53:21 -0400

    feat: Add new icons for navigation

---
+++ src/assets/icons/RightIcon.tsx
@@ -0,0 +1,18 @@
+const RightIcon = () => {
+  return (
+    <svg
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      className="icon-style"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="m8.25 4.5 7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+};
+
+export default RightIcon;
--- src/components/common/sidebar/SideNav.tsx
+++ src/components/common/sidebar/SideNav.tsx
@@ -1,17 +1,57 @@
+import { useState } from "react";
 import "./SideNav.css";
+import HomeIcon from "../../../assets/icons/HomeIcon";
+import ProfileIcon from "../../../assets/icons/ProfileIcon";
+import SettingsIcon from "../../../assets/icons/SettingsIcon";
+import RightIcon from "../../../assets/icons/RightIcon";
+import LeftIcon from "../../../assets/icons/LeftIcon";
 
-const sideNavItems = ["Profile", "Home", "Settings"];
+const sideNavItems = [
+  { index: 0, item: "Home", icon: <HomeIcon /> },
+  {
+    index: 1,
+    item: "Profile",
+    icon: <ProfileIcon />,
+  },
+  {
+    index: 2,
+    item: "Settings",
+    icon: <SettingsIcon />,
+  },
+];
 
 const SideNav = () => {
+  const [sideNavVisible, setSideNavVisible] = useState(false);
+
+  const handleSideNav = () => {
+    // console.log(sideNavVisible);
+    setSideNavVisible(!sideNavVisible);
+  };
+
   return (
     <div className="sidenav">
-      <div className="sidenav_list">
-        {sideNavItems.map((item, index) => (
-          <button key={index} className="bottomBorder">
-            {item}
-          </button>
-        ))}
+      <div onClick={() => handleSideNav()}>
+        {!sideNavVisible ? <RightIcon /> : <LeftIcon />}
       </div>
+
+      {!sideNavVisible && (
+        <div className="sidenav_list_collapsed">
+          {sideNavItems.map((item) => (
+            <div>{item.icon}</div>
+          ))}
+        </div>
+      )}
+
+      {sideNavVisible && (
+        <div className="sidenav_list_full">
+          {sideNavItems.map((item) => (
+            <button key={item.index} className="sidenav_list_item">
+              {item.icon}
+              {item.item}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
